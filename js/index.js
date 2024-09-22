4be3063f93a91378337acb8cf7cd043eb982762a
Raihan <aboraihan14@gmail.com>
2024-09-22 14:52:07 +0600

    maximun seat selected

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
 let btns = document.querySelectorAll('.btn-normal')
+let count = 0 
+let countLeft = 40
 const selectedSeatListing = document.getElementById('selectedSeatListing')
 for(const btn of btns){
     
     btn.addEventListener('click', function(){
         const btnNormal = btn.innerText
         document.getElementById('noSeatSelectedEl').classList.add('hidden')
+
+
+        
+
+        // !count 
+        const seatCount = document.getElementById('seatCount')
+        count++
+       let co =  seatCount.innerText=count  
+
+        // !count end
+
+        
+        if(co > 4){
+            alert("Maximum Seat Selected")
+            return
+             
+           
+        }
+
+
+        // !!seal left 
+
+        const seatLeftCount = document.getElementById('seatLeftCount')
+        countLeft--
+        seatLeftCount.innerText = countLeft
+        // !seal left  end
 
 
         const pTag = document.createElement('p')
